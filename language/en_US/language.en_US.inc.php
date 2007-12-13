b8a6fc79c00425b3dd39f51c136ff126bad71e85
harpiain@gmail.com <harpiain@gmail.com>
2007-12-13 21:32:42 +0000

    Atualização do "trunk"
    - faltou esse arquivo na padronização conforme wiki
      (http://svn.softwarepublico.gov.br/trac/cacic/wiki/cacicTranslate)
    
    
    
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/gerente@242 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ language/en_US/language.en_US.inc.php
@@ -0,0 +1,17 @@
+en_US     language_en-us                                                                                      Geral     info      en-US     
+en_US     language_es                                                                                         Geral     info      es        
+en_US     language_fr                                                                                         Geral     info      fr        
+en_US     geral                                                                                               Geral     TagHeader           General
+en_US     language                                                                                            Geral     info                Language
+en_US     kciq_msg access level                                                                               Geral     info                Access level
+en_US     kciq_menu fast search                                                                               Geral                         Search
+en_US     kciq_menu search                                                                                    Geral     info                Search
+en_US     language_pt-br                                                                                      Geral     info      pt-BR     
+en_US     kciq_msg user                                                                                       Geral     info                User
+en_US     instalador                                                                                          InstaladorTagHeader           Installer
+en_US     kciq_installertitle                                                                                 instaladorinfo                CACIC Web Installer
+en_US     kciq_installerintrotitle                                                                            instaladorinfo                Introdution
+en_US     kciq_installerresources                                                                             instaladorinfo                Installer features:
+en_US     kciq_mnt_lang traducao                                                                              Manutençãoinfo                Language
+en_US     manutenção                                                                                          ManutençãoTagHeader           Maintenance
+en_US     kciq_mnt_tradutor                                                                                   Manutençãoinfo                CACIC Language translate
