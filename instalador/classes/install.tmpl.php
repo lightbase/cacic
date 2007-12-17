170a6d182a57cdeb06de36263d51b67b87a3f22f
harpiain@gmail.com <harpiain@gmail.com>
2007-12-17 02:25:10 +0000

    Atualização do "trunk"
    - implementação da tradução do instalador
    
    
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/gerente@248 fecfc0c7-e812-0410-ae72-849f08638ee7

--- instalador/classes/install.tmpl.php
+++ instalador/classes/install.tmpl.php
@@ -47,15 +47,26 @@ Class Template extends patTemplate {
      $this->readTemplatesFromInput('install_footer.tmpl');
      $this->readTemplatesFromInput('install_navbar.tmpl');
      
-     $this->addVar('tmplPageHeader', 'CACIC_TITLE', 'CACIC - Instalador' );
+     $this->addVar('tmplPageHeader', 'CACIC_TITLE', $this->oLang->_('kciq_msg web_installer') );
      $this->addVar('tmplPageHeader', 'CACIC_LANG', CACIC_LANGUAGE );
+     $this->addVar('tmplPageBody', 'KCIQ_DEF_VERSION', $this->oLang->_('kciq_msg def_version') );
      $this->addVar('tmplPageBody', 'CACIC_VERSION', CACIC_VERSION );
+     $this->addVar('tmplPageBody', 'KCIQ_WEB_INSTALLER', $this->oLang->_('kciq_msg web_installer') );
+     $this->addVar('tmplPageBody', 'KCIQ_JS_ENABLE', $this->oLang->_('kciq_msg js_enable') );
      $this->addVar('tmplPageHeader', 'CACIC_LANG_CHARSET', CACIC_LANG_CHARSET );
      $this->addVar('tmplPageHeader', 'CACIC_THEME', CACIC_THEME );
      $this->addVar('tmplPageHeader', 'CACIC_URL', CACIC_URL_INSTALL );
      $this->addVar('tmplNavBar', 'passo', 'first' );
+     $this->addVar('tmplNavBarPreInstall',  'KCIQ_NEXT', $this->oLang->_('kciq_msg next'));
+     $this->addVar('tmplNavBarLicenca',  'KCIQ_NEXT', $this->oLang->_('kciq_msg next'));
+     $this->addVar('tmplNavBarLicenca',  'KCIQ_PREVIOUS', $this->oLang->_('kciq_msg previous'));
+     $this->addVar('tmplNavBarLicenca',  'KCIQ_LICENSE_AGREE_MSG', $this->oLang->_('kciq_msg aceito'));
+     $this->addVar('tmplNavBarLicenca',  'KCIQ_LICENSE_AGREE', $this->oLang->_('kciq_msg aceitar'));
      $this->addVar('tmplIntroducao', 'kciq_installerintrotitle', $this->oLang->_('kciq_installerintrotitle'));
      $this->addVar('tmplIntroducao', 'kciq_installer_introdution', $this->oLang->_('kciq_installer_introdution'));
+     $this->addVar('tmplIntroducao', 'KCIQ_DEF_LANGUAGE', $this->oLang->_('kciq_msg def_language'));
+     $this->addVar('tmplStatusBar', 'KCIQ_MSG', $this->oLang->_('kciq_msg mensagem'));
+     $this->addVar('tmplPageFooter', 'KCIQ_APOIO', $this->oLang->_('kciq_msg apoio'));
    }
 
    function header() {
--- language/pt_BR/language.pt_BR.inc.php
+++ language/pt_BR/language.pt_BR.inc.php
@@ -1,20 +1,30 @@
+pt_BR     kciq_msg aceitar                                                                                    instalador                    Aceitar
+pt_BR     kciq_msg aceito                                                                                     instalador                    Aceito a licença
+pt_BR     kciq_msg apoio                                                                                      Geral                         Apoio colaborativo
 pt_BR     language_en-us                                                                                      Geral     info      en-US     English (US)
 pt_BR     language_es                                                                                         Geral     info      es        Español
 pt_BR     language_fr                                                                                         Geral     info      fr        Francè
 pt_BR     geral                                                                                               Geral     TagHeader           Geral
 pt_BR     language                                                                                            Geral     info                Idioma
+pt_BR     kciq_msg def_language                                                                               instaladorinfo                Idioma
+pt_BR     kciq_mnt_lang traducao                                                                              Manutençãoinfo                Idioma a traduzir
+pt_BR     instalador                                                                                          InstaladorTagHeader           Instalador
+pt_BR     kciq_installertitle                                                                                 instaladorinfo                Instalador WEB para o CACIC
+pt_BR     kciq_msg web_installer                                                                              instaladorinfo                Instalador WEB para o CACIC
+pt_BR     kciq_installerintrotitle                                                                            instaladorinfo                Introdução
 pt_BR     kciq_msg login                                                                                      Geral     info                Login
 pt_BR     kciq_msg logout                                                                                     Geral     info                Logoff
+pt_BR     manutenção                                                                                          ManutençãoTagHeader           Manutenção
+pt_BR     kciq_msg mensagem                                                                                   Geral                         Mensagem
+pt_BR     kciq_msg js_enable                                                                                  instaladorinfo                Necessário ativar <b>JavaScript</b> para usar o Instalador Web
 pt_BR     kciq_msg access level                                                                               Geral     info                Nível de acesso
 pt_BR     kciq_menu fast search                                                                               Geral                         Pesquisa rápida
 pt_BR     kciq_menu search                                                                                    Geral     info                Pesquisar
-pt_BR     language_pt_BR                                                                                      Geral     info      pt_BR     Português Brasileiro
-pt_BR     kciq_msg user                                                                                       Geral     info                Usuário
-pt_BR     instalador                                                                                          InstaladorTagHeader           Instalador
-pt_BR     kciq_installertitle                                                                                 instaladorinfo                Instalador WEB para o CACIC
-pt_BR     kciq_installerintrotitle                                                                            instaladorinfo                Introdução
+pt_BR     language_pt_br                                                                                      Geral     info      pt_BR     Português Brasileiro
+pt_BR     kciq_msg next                                                                                       Geral     info                Próximo
 pt_BR     kciq_installerresources                                                                             instaladorinfo                Recursos do Instalador:
-pt_BR     kciq_installer_introdution                                                                          instaladorarquivo             introducao.html
-pt_BR     kciq_mnt_lang traducao                                                                              Manutençãoinfo                Idioma a traduzir
-pt_BR     manutenção                                                                                          ManutençãoTagHeader           Manutenção
 pt_BR     kciq_mnt_tradutor                                                                                   Manutençãoinfo                Tradução de texto do CACIC
+pt_BR     kciq_msg user                                                                                       Geral     info                Usuário
+pt_BR     kciq_msg def_version                                                                                instaladorinfo                Versão
+pt_BR     kciq_installer_introdution                                                                          instaladorarquivo             introducao.html
+pt_BR     kciq_msg previous                                                                                   instalador                    Anterior
