71233784bdb149a6dc24dfd95bd63489482f6628
harpiain@gmail.com <harpiain@gmail.com>
2008-11-07 01:44:52 +0000

    - Exclusão de arquivo desnecessário - atualização de scripts de criação/atualização de BD
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/gerente@620 fecfc0c7-e812-0410-ae72-849f08638ee7

--- instalador/sql/cacic_fev2006.sql
+++ instalador/sql/cacic_fev2006.sql
@@ -395,4 +395,78 @@ INSERT INTO `patrimonio_config_interface`
         (`id_local`, `id_etiqueta`, `nm_etiqueta`, `te_etiqueta`, `in_exibir_etiqueta`, `te_help_etiqueta`,
          `te_plural_etiqueta`, `nm_campo_tab_patrimonio`, `in_destacar_duplicidade`)
      VALUES
-        (1, 'etiqueta1a', 'Etiqueta 1a', 'Linha de Negócio', 'S', 'Selecione a Linha de Negócio', 'Linhas de Negócio', 'id_unid_organizacional_nivel1a', 'N');
+        (1, 'etiqueta1a', 'Etiqueta 1a', 'Linha de Negócio', 'S', 'Selecione a Linha de Negócio', 'Linhas de Negócio', 'id_unid_organizacional_nivel1a', 'N');-- ---------------------------------------------------------------------------------------------
+-- Estrutura da tabela `srcacic_sessoes`
+-- Armazenamento de sessoes de Suporte Remoto Seguro
+-- Contera os dados dos usuarios visitante e visitado e suas sessoes para fins de suporte remoto
+-- ---------------------------------------------------------------------------------------------
+CREATE TABLE `srcacic_sessoes` 
+	(
+	`id_sessao` int(11) NOT NULL auto_increment,
+	`dt_hr_inicio_sessao` datetime NOT NULL,
+	`dt_hr_fim_sessao` datetime default NULL,
+	`id_usuario_visitante` int(11) NOT NULL default '0',
+	`nm_nome_acesso_visitado` varchar(30) character set utf8 NOT NULL,
+	`nm_nome_completo_visitado` varchar(100) NOT NULL default 'NoNoNo',
+	`te_node_address_visitado` varchar(17) character set utf8 NOT NULL,
+	`id_so_visitado` int(11) NOT NULL,
+	`te_node_address_visitante` varchar(17) character set utf8 NOT NULL default 'NoNoNo',
+	`dt_hr_ult_contato` datetime default NULL,
+	PRIMARY KEY  (`id_sessao`),
+		KEY `idx_idUsuario` (`id_usuario_visitante`),
+		KEY `idx_dtHrInicioSessao` (`dt_hr_inicio_sessao`)
+	) 
+ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Log de Atividades no Sistema CACIC' AUTO_INCREMENT=1 ;
+
+-- -----------------------------------------------------------------------------------------------------------
+-- Estrutura da tabela `dominios`
+-- Armazenamento de dados de servidores de dominios
+-- Esses dados serao utilizados nas autenticacoes de usuarios e criacao de sessoes para fins de suporte remoto
+-- -----------------------------------------------------------------------------------------------------------
+CREATE TABLE `dominios` 
+	(
+	`id_dominio` int(11) NOT NULL auto_increment,
+      `nm_dominio` varchar(60) NOT NULL,
+	`te_ip_dominio` varchar(15) NOT NULL,
+	`id_tipo_protocolo` varchar(20) NOT NULL,
+	`nu_versao_protocolo` varchar(10) NOT NULL,
+	`te_string_DN` varchar(100) NOT NULL,
+	`te_observacao` text NOT NULL,
+	`in_ativo` char(1) NOT NULL default 'S',
+	PRIMARY KEY  (`id_dominio`)
+	) 
+ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Servidores de Dominio para Autenticacao do srCACIC' AUTO_INCREMENT=2 ;
+
+-- -----------------------------------------------------------------------------------------------------------
+-- Insercao da coluna `id_dominio`
+-- Relacionamento de redes com servidores de dominios
+-- -----------------------------------------------------------------------------------------------------------
+ALTER TABLE `redes` ADD `id_dominio` INT( 11 ) NULL AFTER `id_local` ;
+
+-- -----------------------------------------------------------------------------------------------------------
+-- Insercao da coluna `id_dominio`
+-- Relacionamento de usuarios com servidores de dominios
+-- -----------------------------------------------------------------------------------------------------------
+ALTER TABLE `usuarios` ADD `id_dominio` INT( 11 ) NULL AFTER `id_local` 
+
+-- -----------------------------------------------------------------------------------------------------------
+-- Redimensionamento de coluna `nm_modulo` para armazenamento de nomes maiores
+-- -----------------------------------------------------------------------------------------------------------
+ALTER TABLE `redes_versoes_modulos` CHANGE `nm_modulo` `nm_modulo` VARCHAR( 100 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL  
+
+-- -------------------------------------------------------------------------------------------------------------
+-- Insercao de coluna `in_mswindows` para classificacao do Sistema Operacional pelo tipo
+-- Essa informacao sera usada principalmente na resposta do Gerente WEB aos Agentes quando estes fizerem contato
+-- -------------------------------------------------------------------------------------------------------------
+ALTER TABLE `so` ADD `in_mswindows` CHAR( 1 ) NOT NULL DEFAULT 'S';
+
+-- ----------------------------------------------------------------------------------------------------------------------------------
+-- Redimensionamento de coluna `nm_unid_organizacional_nivel1a` para armazenamento de nomes maiores de linhas de negocio ou similares
+-- ----------------------------------------------------------------------------------------------------------------------------------
+ALTER TABLE `unid_organizacional_nivel1a` CHANGE `nm_unid_organizacional_nivel1a` `nm_unid_organizacional_nivel1a` VARCHAR( 100 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL
+
+-- ----------------------------------------------------------------------------------------------------------------------
+-- Redimensionamento de coluna `nm_unid_organizacional_nivel2` para armazenamento de nomes maiores de orgaos ou similares
+-- ----------------------------------------------------------------------------------------------------------------------
+ALTER TABLE `unid_organizacional_nivel2`  CHANGE `nm_unid_organizacional_nivel2`  `nm_unid_organizacional_nivel2`  VARCHAR( 100 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL  
+
