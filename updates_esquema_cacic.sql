40db4cd950d307391a6a9855866bfb110e59c5c7
harpiain@gmail.com <harpiain@gmail.com>
2009-02-27 01:26:28 +0000

    - Exclusão de arquivos desnecessários por haver diretório específico para sql
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/trunk/gerente@735 fecfc0c7-e812-0410-ae72-849f08638ee7

--- updates_esquema_cacic.sql
+++
@@ -1,3 +0,0 @@
- ALTER TABLE `unid_organizacional_nivel1a` CHANGE `nm_unid_organizacional_nivel1a` `nm_unid_organizacional_nivel1a` VARCHAR( 100 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL
-
-  ALTER TABLE `unid_organizacional_nivel2` CHANGE `nm_unid_organizacional_nivel2` `nm_unid_organizacional_nivel2` VARCHAR( 100 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL  
