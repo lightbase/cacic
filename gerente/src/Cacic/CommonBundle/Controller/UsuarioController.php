fe04074cd4084523d1adb92594a8d119dda7d53f
gedeson <gedeson@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-01-18 19:09:27 +0000

    Failed commit: Default

--- gerente/src/Cacic/CommonBundle/Controller/UsuarioController.php
+++ gerente/src/Cacic/CommonBundle/Controller/UsuarioController.php
@@ -13,7 +13,10 @@ class UsuarioController extends Controller
 	 */
     public function indexAction()
     {
-        return $this->render('CacicCommonBundle:Default:index.html.twig', array('name' => $name));
+       	 return $this->render('CacicCommonBundle:Default:index.html.twig');
+       	 
+       	 
+
     }
     
     /**
@@ -22,9 +25,35 @@ class UsuarioController extends Controller
      * Caso o idUsuario não seja informado, carrega os dados do usuário logado.
      * @param int $idUsuario
      */
-    public function trocasenhaAction($idUsuario)
+    public function trocarsenhaAction($idUsuario)
     {
     	$objUsuario = $this->getDoctrine()->getRepository('CacicCommonBundle:Usuarios')->find( $idUsuario );
     	echo"<pre>";var_dump($objUsuario);die;
+    }
+
+    /**
+     * Página de Cadastrar novo usuario.
+     *
+     */
+    public function cadastrarAction()
+    {
+		return $this->render('CacicCommonBundle:Default:index.html.twig');
+    }
+
+    /**
+     *  Página de editar dados do Usuario
+     *  @param int $idusuario
+     */
+    public function editarAction($idUsuario)
+    {
+		return $this->render('CacicCommonBundle:Default:index.html.twig');
+    }
+
+    /**
+     *  Página de recuperação de senha
+     */
+    public function recuperarsenhaAction()
+    {
+
     }
 }
