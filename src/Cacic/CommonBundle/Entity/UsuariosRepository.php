bafe12466db17ca8d936e2b61d9db4109a8f90d2
gedeson <gedeson@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-01-31 15:54:19 +0000

    Failed commit: Default
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1029 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/Cacic/CommonBundle/Entity/UsuariosRepository.php
+++ src/Cacic/CommonBundle/Entity/UsuariosRepository.php
@@ -40,4 +40,8 @@ class UsuariosRepository extends EntityRepository
 
     }
 
+    public function gerarsenha()
+    {
+
+    }
 }
--- src/Cacic/CommonBundle/Form/Type/UsuarioType.php
+++ src/Cacic/CommonBundle/Form/Type/UsuarioType.php
@@ -4,62 +4,77 @@ namespace Cacic\CommonBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class  UsuarioType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
-              $builder
-                ->add('idLocal', 'entity', array(
-                    'empty_value' => 'Selecione Local',
-                    'class' => 'CacicCommonBundle:Locais',
-                    'property' => 'nmlocal',
-                    'label'=> 'Local:',
-                 ))
-                ->add('TeLocaisSecundarios', 'entity', array(
-                    'class' => 'CacicCommonBundle:Usuarios',
-                    'property' => 'TeLocaisSecundarios',
-                    'multiple' => true,
-                    'label'=> 'Local Secundarios:',
-                 ))
-                ->add('NmUsuarioAcesso', 'text', array(
-                    'label' => 'Login:',
-                 ))
-                ->add('nmUsuarioCompleto', 'text', array(
-                    'label'=> 'Nome Completo:',
-                 ))
-                ->add('teEmailsContato', 'email', array(
-                    'label' => 'Email:',
-                 ))
-                ->add('teTelefonesContato', 'number', array(
-                  'required' => false,
-                    'label' => 'Telefone para Contato:',
-                 ))
+              $builder->add('idLocal', 'entity',
+                            array(
+                            'empty_value' => 'Selecione Local',
+                            'class' => 'CacicCommonBundle:Locais',
+                            'property' => 'nmlocal',
+                            'label'=> 'Local:',
+              ));
 
-                ->add('Grupo', 'entity', array(
-                  'empty_value' => 'Selecione Acesso',
-                  'class' => 'CacicCommonBundle:GrupoUsuarios',
-                  'property' => 'teGrupoUsuarios',
-                  'label'=> 'Selecione Tipo de Acesso:'
-                 ))
-                  ->add('idGrupoUsuarios', 'textarea', array(
-                  'required' => false,
-                  'label' => 'Descrição do tipo de acesso:',
-                  'max_length' => '255',
-              ))
+              $builder->add('idServidorAutenticacao', 'hidden',
+                            array( "data"=> '1'
+               ));
+              $builder->add('TeLocaisSecundarios', 'entity',
+                            array(
+                            'class' => 'CacicCommonBundle:Usuarios',
+                            'property' => 'TeLocaisSecundarios',
+                            'multiple' => true,
+                            'label'=> 'Local Secundarios:',
+               ));
 
-                  ->getForm();
+              $builder->add('nmUsuarioAcesso', 'text',
+                             array(
+                            'label' => 'Login:',
+               ));
 
+              $builder ->add('nmUsuarioCompleto', 'text',
+                              array(
+                             'label'=> 'Nome Completo:',
+               ));
 
+              $builder->add('teEmailsContato', 'email',
+                            array(
+                            'label' => 'Email:',
+               ));
+
+              $builder->add('teTelefonesContato', 'number',
+                            array(
+                            'required' => false,
+                            'label' => 'Telefone para Contato:',
+              ));
+
+              $builder->add('teSenha', 'password',
+                            array(
+                            'required' => false,
+                            'label' => 'Senha:'
+              ));
+
+              $builder->add('Grupo', 'entity',
+                            array(
+                          'empty_value' => 'Selecione Acesso',
+                          'class' => 'CacicCommonBundle:GrupoUsuarios',
+                          'property' => 'teGrupoUsuarios',
+                          'label'=> 'Selecione Tipo de Acesso:'
+              ));
+
+              $builder->add('idGrupoUsuarios', 'textarea',
+                            array(
+                             'required' => false,
+                             'label' => 'Descrição do tipo de acesso:',
+                             'max_length' => '255',
+              ));
     }
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-      {
-        $resolver->setDefaults(array(
-            'data_class' => 'Cacic\CommonBundle\Entity\Usuarios',
-        ));
-      }
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\Form.FormTypeInterface::getName()
+     */
     public function getName()
      {
         return 'usuario';
