0d2ae00f59394f8e4c4edbdefa49a3309a4065ba
eciobond <eciobond@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-02-22 00:15:38 +0000

    - ConfiguraçãoPadrão - FontAwesome
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1062 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ src/Cacic/CommonBundle/Controller/ConfiguracaoPadraoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cacic\CommonBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Cacic\CommonBundle\Entity\ConfiguracoesPadrao;
+use Cacic\CommonBundle\Form\Type\ConfiguracaoPadraoType;
+
+/**
+ * 
+ * CRUD da Entidade Locais
+ * @author lightbase
+ *
+ */
+class ConfiguracaoPadraoController extends Controller
+{
+	
+	public function indexAction()
+	{
+		
+	}
+	
+}
