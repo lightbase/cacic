0d559b792b553ae5741bf68d12ab0c6bab2e765d
eduardo.santos <eduardo.santos@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-03-21 23:42:46 +0000

    Adição de tema que estava quebrado
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1105 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ web/bundles/caciccommon/js/System.js
@@ -0,0 +1,37 @@
+/**
+ * Biblioteca com métodos comuns a todo o Sistema
+ */
+
+var System = {
+	Grid : { // Comportamentos relacionados a itens de grids de listagens de itens
+		excluir : function(){ // Método executado ao acionar a funcionalidade de exclusão
+			$( "a.bt-excluir" ).bind( 'click', function(e){
+				e.preventDefault();
+				var url = $( this ).attr( 'href' );
+				var id = $( this ).parent().parent().attr( 'id' ).replace( /.*?(\d+)$/, '$1' );
+				$( "#System_Excluir" ).data( 'params', { 'url' : url, 'id' : id } ).dialog( "open" );
+			});
+		}
+	},
+	Flash : { // Comportamentos relacionados a mensagens
+		show : function( type, msg ){ // Método executado na exibição de mensagens do sistema
+			$( '.alert' ).fadeOut();
+			/**
+			 * @todo alterar para permitir array de mensagens
+			 */
+			$( '#msg' +type ).append( msg + '<br />' ).fadeIn();
+		}
+	},
+    Form : { // Comportamentos relacionados a formulários
+        reset : function(){ // Método executado ao se acionar a funcionalidade de "limpar valores" do formulário
+            $( 'button[type=reset],input[type=reset]' ).click(function(){
+                $( 'input[type=text]').attr('placeholder', '');
+            });
+        }
+    },
+	Menu : {
+		setActive : function( url ){
+			//alert( url );
+		}
+	}
+}
---
+++ web/bundles/caciccommon/js/init.js
@@ -0,0 +1,65 @@
+/**
+ * Eventos inicializados após renderização da página
+ */
+$(document).ready(function(){
+	/**
+	 * Remove o COOKIE com informação da imagem de BACKGROUND
+	 */
+	$.cookie("AdminIntensoBackground", null );
+	
+	/**
+	 * Animação AJAX
+	 */
+	$( document ).ajaxStart( function() { 
+		$( '#ajaxLoading' ).fadeIn();
+		$( 'body' ).css( 'overflow', 'hidden' );
+	}) 
+	.ajaxStop( function() {
+		$( '#ajaxLoading' ).fadeOut();
+		$( 'body' ).css( 'overflow', 'auto' );
+	});
+	
+	/**
+	 * Modal de confirmação de exclusão de item
+	 */
+	$( "#System_Excluir" ).dialog({
+		autoOpen: false,
+		height: 190,
+		width: 350,
+		modal: true,
+		closeText: 'Fechar',
+		buttons: {
+			"Excluir" : function(){
+				$( this ).dialog( "close" );
+				var params = $( this ).data( 'params' );
+				$.ajax(
+					{
+						type: "POST",
+						url: params.url,
+						cache: false,
+						async: false,
+						success: function( data )
+						{
+						    System.Flash.show( 'Sucesso', 'Item excluído com sucesso!' );
+						    $( '#item_' + params.id ).fadeOut(); // Remove o item da Grid
+						},
+						error: function( data )
+						{
+							System.Flash.show( 'Erro', 'Erro na exclusão do item!' );
+						},
+						data: params
+					}
+				);
+			},
+			"Cancelar" : function(){
+				$( this ).dialog( "close" );
+			}
+		},
+		show: { effect: "fade", duration: 500 },
+		hide: { effect: "fade", duration: 500 }
+	});
+	
+	System.Grid.excluir(); // Inicializa o LISTENER para os botões-padrão de exclusão de itens
+    System.Form.reset(); // Inicializa o LISTENER para os botões (ou input) type=reset
+    System.Menu.setActive( window.location.toString() ); // Configura o menu ativo
+});
