15b034f5950c9077c48520a3cbfc3befff2707c2
legendoliveira <legendoliveira@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-04-03 18:36:07 +0000

    arquivo pagina supercacic

--- integrador/src/WSCacicNeo/wscacicneo/static/main.js
+++ integrador/src/WSCacicNeo/wscacicneo/static/main.js
@@ -1,5 +1,7 @@
-
 Ext.onReady(function() {
+
+	var links_html = '<a href="relatorios"></a>';	
+
 
 	var mainMenu = Ext.create('Ext.menu.Menu', {
 	  	renderTo: 'menu-principal',
@@ -10,27 +12,77 @@ Ext.onReady(function() {
 		title: 'Home',
         titleAlign: 'center',
 		items: [
-			{text: 'Item 1'	},
-			{text: 'Item 2'	},
-			{text: 'Item 3'	},
-			{text: 'Item 4'	},
-			{text: 'Item 5' },
-			{text: 'Item 6'	},
-			{text: 'Item 7'	},
-			{text: 'Item 8'	},
-			{text: 'Item 9'	},
-			{text: 'Item 10'}]
+			{ xtype: 'box',
+  			  autoEl: {
+    			  tag: 'a',
+    			  href: 'home',
+    			  cn: 'Home'
+  					}},
+			{xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'dashboard',
+    			cn: 'Dashboard'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'diagnostico',
+    			cn: 'Diagnosticos'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'administracao',
+    			cn: 'Administraçao'
+  					}},
+			{xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'pagina1',
+    			cn: 'Pagina1'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'pagina2',
+    			cn: 'Pagina2'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'pagina3',
+    			cn: 'Pagina3'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'pagina4',
+    			cn: 'Pagina4'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {
+    			tag: 'a',
+    			href: 'pagina5',
+    			cn: 'Pagina5'
+  					}},
+			{ xtype: 'box',
+  			autoEl: {	
+    			tag: 'a',
+    			href: 'pagina6',
+    			cn: 'Pagina6'
+  					}}]
 	});
 
-	fav_html = '<div id="favoriteItems">'+
-	 	'<div class="fav"><a><img src="static/icons/estatisticas.png">Estatísticas</a></div>'+
-	 	'<div class="fav"><a><img src="static/icons/busca.png">Busca</a></div>'+
-	 	'<div class="fav"><a><img src="static/icons/downloads.png">Downloads</a></div>'+
+   var	fav_html = '<div id="favoriteItems">'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/estatisticas.png">Estatísticas</a></div>'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/busca.png">Busca</a></div>'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/downloads.png">Downloads</a></div>'+
 	 	'<div class="fav"><a href="relatorios"><img src="static/icons/relatorios.png">Relatórios</a></div>'+
-	 	'<div class="fav"><a><img src="static/icons/mensagens.png">Mensagens</a></div>'+
-	 	'<div class="fav"><a><img src="static/icons/ajuda.png">Ajuda</a></div>'+
-	 	'<div class="fav"><a><img src="static/icons/usuario.png">Usuário</a></div>'+
-	 	'<div class="fav"><a><img src="static/icons/ferramentas.png">Ferramentas de sistema</a></div>'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/mensagens.png">Mensagens</a></div>'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/ajuda.png">Ajuda</a></div>'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/usuario.png">Usuário</a></div>'+
+	 	'<div class="fav"><a href="relatorios"><img src="static/icons/ferramentas.png">Ferramentas de sistema</a></div>'+
 	'</div>';
 
 	var favoriteMenu = Ext.create('Ext.panel.Panel', {
