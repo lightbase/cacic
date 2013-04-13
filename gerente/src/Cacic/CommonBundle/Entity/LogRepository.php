1d8621493fb016d81c710528b2b1440fc58601d0
gedeson <gedeson@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-04-13 12:32:26 +0000

    Ajustes

--- gerente/src/Cacic/CommonBundle/Entity/LogRepository.php
+++ gerente/src/Cacic/CommonBundle/Entity/LogRepository.php
@@ -18,17 +18,16 @@ class LogRepository extends EntityRepository
 	 * Realiza pesquisa por LOGs segundo parâmetros informados
 	 * @param array $data
 	 */
-    public function pesquisar( $dataInicio, $dataFim, $locais )
+    public function acessoPesquisar( $dataInicio, $dataFim, $locais )
     {
+        $filtros = array();
+        if ( $dataInicio )	$filtros[] = 'log.dtAcao >= :dtInicio';
+        if ( $dataFim)	$filtros[] = 'log.dtAcao <= :dtFim';
+        if ( count($locais) ) $filtros[] = 'loc.idLocal IN (:idLocal)';
 
-    	$filtros = array();
-    	if ( $dataInicio )	$filtros[] = 'log.dtAcao >= :dtInicio';
-    	if ( $dataFim)	$filtros[] = 'log.dtAcao <= :dtFim';
-       	if ( count($locais) ) $filtros[] = 'loc.idLocal IN (:idLocal)';
-    	
-    	if ( count( $filtros ) ) $filtros = 'WHERE '. implode( ' AND ', $filtros );
-    	else $filtros = '';
-    	
+        if ( count( $filtros ) ) $filtros = 'WHERE '."log.csAcao = 'ACE'  AND ". implode( ' AND ', $filtros );
+        else $filtros = '';
+
         $_dql = "SELECT log, usr.nmUsuarioCompleto, loc.nmLocal
 				FROM CacicCommonBundle:Log log
 				LEFT JOIN log.idUsuario usr
@@ -36,11 +35,41 @@ class LogRepository extends EntityRepository
 				{$filtros}";
 
         $query = $this->getEntityManager()->createQuery( $_dql )
+
+
         									->setParameter('dtInicio', $dataInicio)
         									->setParameter('dtFim', $dataFim)
         									->setParameter('idLocal', $locais);
 
         return $query->getArrayResult();
+
+
+    }
+    public function AtividadePesquisar( $dataInicio, $dataFim, $locais )
+    {
+        $filtros = array();
+        if ( $dataInicio )	$filtros[] = 'log.dtAcao >= :dtInicio';
+        if ( $dataFim)	$filtros[] = 'log.dtAcao <= :dtFim';
+        if ( count($locais) ) $filtros[] = 'loc.idLocal IN (:idLocal)';
+
+        if ( count( $filtros ) ) $filtros = 'WHERE '."log.csAcao <> 'ACE'  AND ". implode( ' AND ', $filtros );
+        else $filtros = '';
+
+
+        $_dql = "SELECT log, usr.nmUsuarioCompleto, loc.nmLocal
+				FROM CacicCommonBundle:Log log
+				LEFT JOIN log.idUsuario usr
+				LEFT JOIN usr.idLocal loc
+				{$filtros}";
+
+        $query = $this->getEntityManager()->createQuery( $_dql )
+            ->setParameter('dtInicio', $dataInicio)
+            ->setParameter('dtFim', $dataFim)
+            ->setParameter('idLocal', $locais);
+
+
+        return $query->getArrayResult();
+
 
 
     }
