f3ea2da178269213aebb5b8908132e57a8c84128
eciobond <eciobond@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-05-03 06:09:45 +0000

    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1179 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/Cacic/CommonBundle/Controller/PatrimonioConfigInterfaceController.php
+++ src/Cacic/CommonBundle/Controller/PatrimonioConfigInterfaceController.php
@@ -2,6 +2,8 @@
 
 namespace Cacic\CommonBundle\Controller;
 
+use Cacic\CommonBundle\Entity\PatrimonioConfigInterfaceRepository;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -44,7 +46,6 @@ class PatrimonioConfigInterfaceController extends Controller
 
         $form = $this->createForm( new PatrimonioConfigInterfaceType(), $patrimonio );
 
-
         if ( $request->isMethod('POST') )
         {
             $form->bind( $request );
@@ -65,26 +66,30 @@ class PatrimonioConfigInterfaceController extends Controller
 	 * 
 	 * Tela de edição de opções de Coleta de informações patrimoniais e localização física
 	 */
-	public function opcoesAction(Request $request)
+	public function opcoesAction( Request $request )
 	{
-        $opcoes = new PatrimonioConfigInterface();
-        $form = $this->createForm( new OpcoesType(), $opcoes );
-
-        if ( $request->isMethod('POST') )
-        {
-            $form->bind( $request );
-
-            if ( $form->isValid() )
-            {
-                $this->getDoctrine()->getManager()->persist( $opcoes );
-                $this->getDoctrine()->getManager()->flush();
-
-                $this->get('session')->getFlashBag()->add('success', 'Dados salvos com sucesso!');
-
-                return $this->redirect( $this->generateUrl( 'cacic_patrimonio_opcoes') );
-            }
+		if ( $request->isMethod('POST') )
+        { // Se dados foram submetidos
+        	$_data = $request->get('config');
+        	
+        	$this->getDoctrine()->getRepository( 'CacicCommonBundle:PatrimonioConfigInterface' )->atualizarOpcoesDestacarDuplicidade( array_keys($_data), $_data['idLocal'] );
+        	$this->get('session')->getFlashBag()->add('success', 'Dados salvos com sucesso!');
         }
-        return $this->render( 'CacicCommonBundle:PatrimonioConfigInterface:opcoes.html.twig', array( 'form' => $form->createView() ) );
+		
+		/**
+         *
+         * @todo no caso de ser um usuário administrativo, exibir lista com todos os locais cadastrados
+         * @var int
+         */
+        $local = $this->getUser()->getIdLocal(); // Recupera o Local da sessão do usuário logado
+
+        return $this->render(
+        	'CacicCommonBundle:PatrimonioConfigInterface:opcoes.html.twig', 
+        	array(
+        		'opcoes' => $this->getDoctrine()->getRepository( 'CacicCommonBundle:PatrimonioConfigInterface' )->getOpcoesDestaqueDuplicidade( $local ),
+        		'local' => $local
+        	)
+        );
 	}
 	
 }
