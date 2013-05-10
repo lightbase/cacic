e7412fb06d2ed108fac6d4d3ffca1bea79b1e891
paulo_borges_ccb@hotmail.com <paulo_borges_ccb@hotmail.com@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-05-10 17:41:11 +0000

    Acrescentado Funcionalidade: Excluir Softwares não usados

--- gerente/src/Cacic/CommonBundle/Resources/public/js/System.js
+++ gerente/src/Cacic/CommonBundle/Resources/public/js/System.js
@@ -11,6 +11,19 @@ var System = {
 				var id = $( this ).parent().parent().attr( 'id' ).replace( /.*?(\d+)$/, '$1' );
 				$( "#System_Excluir" ).data( 'params', { 'url' : url, 'id' : id } ).dialog( "open" );
 			});
+            $( "a.bt-excluir-check" ).bind( 'click', function(e){//Responsável pela exclusão de varios checkbox
+                e.preventDefault();
+                var url = $( this ).attr( 'href' );
+                var result = $("input.tipo-check:checked");  //verifica no input na classe tipo-check se esta checked (marcado)
+                var i;
+                var arrId=[];
+
+                for (i=0;i<result.length;i++) //percorre todos resultados marcados
+                {
+                    arrId[i] = result[i].value;
+                }
+                $( "#System_Excluir" ).data( 'params', { 'url' : url, 'arrId' : arrId } ).dialog( "open" );
+            });
 		}
 	},
 	Flash : { // Comportamentos relacionados a mensagens
@@ -29,6 +42,7 @@ var System = {
                 $( 'input[type=text]').attr('placeholder', '');
             });
         }
+
     },
 	Menu : {
 		setActive : function( url ){
@@ -36,3 +50,18 @@ var System = {
 		}
 	}
 }
+
+//Ao clicar no INPUT do tipo checkbox com id = checkAll, marca e desmarca todos checkbox
+$(document).ready(function() {
+	$('#checkAll').click(function() {
+		if(this.checked == true){
+			$("input[type=checkbox]").each(function() {
+				this.checked = true; 
+			});
+		} else {
+			$("input[type=checkbox]").each(function() {
+				this.checked = false;
+			});
+		}
+	});	
+});
