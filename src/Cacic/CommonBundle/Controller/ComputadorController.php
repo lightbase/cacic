363f208367a1deefde6244ebdb8c8e4d71b7b6a7
eciobond <eciobond@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-06-11 14:35:12 +0000

    dump dos assetics
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1207 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ src/Cacic/CommonBundle/Controller/ComputadorController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cacic\CommonBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * 
+ * @author lightbase
+ *
+ */
+class ComputadorController extends Controller
+{
+	
+	public function filtrarAction()
+	{
+		
+	}
+	
+	public function navegarAction()
+	{
+		
+	}
+	
+	public function consultarAction()
+	{
+		
+	}
+	
+	public function excluirAction()
+	{
+		
+	}
+	
+}
