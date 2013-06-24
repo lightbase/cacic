bb63634807585182e587014949f28eceef17f166
paulo_borges_ccb@hotmail.com <paulo_borges_ccb@hotmail.com@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-06-24 20:34:33 +0000

    Adicionado Helper
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1217 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ src/Cacic/CommonBundle/Helper/Criptografia.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Cacic\CommonBundle\Helper;
+
+
+class Criptografia {
+
+    public $key;
+    public $iv;
+
+    //Contrutor padrão privado para evitar instanciação da classe.
+    private function  __construct(){}
+
+    //---------------------------------------------------------------------------------
+    //  Substituir alguns valores inválidos ao tráfego HTTP
+    //
+    //  @return String contendo o string com valores inv�lidos substituidos por v�lidos
+    //---------------------------------------------------------------------------------
+    public function replaceInvalidHTTPChars($pStrString)
+    {
+        $strNewString = str_replace('+'  , '[[MAIS]]'		, $pStrString);
+        $strNewString = str_replace(' '  , '[[ESPACE]]'  	, $strNewString);
+        $strNewString = str_replace('"'  , '[[AD]]'      	, $strNewString);
+        $strNewString = str_replace("'"  , '[[AS]]'      	, $strNewString);
+        $strNewString = str_replace('\\' , '[[BarrInv]]' 	, $strNewString);
+
+        return $strNewString;
+    }
+
+    //------------------------------------------------------------------------------
+    //  Repor valores substituidos durante tr�fego HTTP
+    //
+    //  @return String contendo o string com valores substituidos
+    //------------------------------------------------------------------------------
+    public function replacePseudoTagsWithCorrectChars($pStrString)
+    {
+        // Conven��es Adotadas para as Substitui��es
+        // -----------------------------------------
+        // [[MAIS]]    => Sinal de Mais   => "+" (comumente interpretado como espa�o, prejudicando a decriptografia) (Deve ser substitu�do ANTES da decriptografia!!!!)
+        // [[BarrInv]] => Barra Invertida => "\" (comumente interpretado como ESCAPE na recep��o)
+        // [[AS]]      => Aspa Simples    => "'"
+        // [[AD]]      => Aspa Dupla      => '"'
+        // [[ESPACE]]  => Espa�o          => ' '
+        // =============================================================================================
+
+        $strNewString = str_replace('[[MAIS]]' 		, '+' 	, $pStrString);
+        $strNewString = str_replace('[[ESPACE]]' 	, ' ' 	, $strNewString);
+        $strNewString = str_replace('[[AD]]'     	, '"' 	, $strNewString);
+        $strNewString = str_replace('[[AS]]'     	, "'"	, $strNewString);
+        $strNewString = str_replace('[[BarrInv]]'	, '.'	, $strNewString); // Ao substituir [[BarrInv]] por "\\" tive problemas, prefer� deixar "."
+        return $strNewString;
+    }
+
+
+    public static function enCrypt($pStrPlainData, $pStrCsCipher, $pStrCsCompress, $pIntCompressLevel=0, $pStrPaddingKey = '', $pBoolForceEncrypt = false)
+    {
+        $request = Request::createFromGlobals();
+
+        $pStrCipherKey = $pStrPaddingKey;
+
+        if ((($pStrCsCipher=='1') && !$request->request('cs_debug') || $pBoolForceEncrypt) )
+        {
+            $strResult = base64_encode(@mcrypt_cbc(MCRYPT_RIJNDAEL_128,$key,$pStrPlainData,MCRYPT_ENCRYPT,$iv));
+            $strResult .= '__CRYPTED__';
+        }
+        else
+            $strResult = $pStrPlainData;
+
+        if (($pStrCsCompress == '1' || $pStrCsCompress == '2') && $pIntCompressLevel > 0)
+            $strResult = gzdeflate($strResult,$pIntCompressLevel);
+
+        $strResult = self::replaceInvalidHTTPChars($strResult);
+        return trim($strResult);
+    }
+
+    // ---------------------------------
+    // Metodo para descriptografia
+    // To decrypt values
+    // p_cs_cipher => Y/N
+    // ---------------------------------
+    public static function deCrypt($pStrCriptedData, $pStrCsCipher, $pIntCsUnCompress='0', $pStrPaddingKey = '', $pBoolForceDecrypt = false)
+    {
+        $request = Request::createFromGlobals();
+
+        $pStrCipherKey = $pStrPaddingKey;
+
+        // Bloco de Substituições para antes da Decriptação
+        // ------------------------------------------------
+        // Razão: Dependendo da configuração do servidor, os valores
+        //        enviados, pertinentes é criptografia, tendem a ser interpretados incorretamente.
+        // Obs.:  Vide Lista de Convenções Abaixo
+        // =======================================================================================
+        $ppStrCriptedData = str_ireplace('[[MAIS]]','+',$pStrCriptedData,$countMAIS);
+        // =======================================================================================
+
+        if ( (substr($ppStrCriptedData,-11) == '__CRYPTED__') && ((($pStrCsCipher=='1') && !$request->request('cs_debug')) || $pBoolForceDecrypt))
+        {
+            $ppStrCriptedData = str_replace('__CRYPTED__' , '' 	, $ppStrCriptedData);
+            $strResult = (trim($ppStrCriptedData)<>''?@mcrypt_decrypt(MCRYPT_RIJNDAEL_128,$key,base64_decode($ppStrCriptedData),MCRYPT_MODE_CBC,$iv):'');
+        }
+        else
+            $strResult = $pStrCriptedData;
+
+        // Bloco de Substituições para depois da Decriptação
+        // -------------------------------------------------
+        // Razão: idem acima, porém, com dados pertinentes aos valores a serem recebidos
+        // =============================================================================
+        $strResult = self::replacePseudoTagsWithCorrectChars($strResult);
+        // =============================================================================
+
+        if ($pIntCsUnCompress == '1')
+            $strResult = gzinflate($strResult);
+
+        // Aqui retiro do resultado a ocorrência do preenchimento, caso exista. (o agente Python faz esse preenchimento)
+        if ($pStrPaddingKey <> '')
+        {
+            $char 		= substr($pStrPaddingKey,0,1);
+            $re 		= "/".$char."*$/";
+            $strResult 	= preg_replace($re, "", $strResult);
+        }
+
+        return trim($strResult);
+    }
+
+
+}
---
+++ src/Cacic/CommonBundle/Helper/TagValue.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Classe responsável por trabalhar com as TAGS referentes as classes WMI da coleta do agente
+ * Essas classes WMI definem o tipo do dado coletado.
+ * referencia WMI: http://msdn.microsoft.com/en-us/library/windows/desktop/aa394573(v=vs.85).aspx
+ */
+
+namespace Cacic\CommonBundle\Helper;
+
+
+class TagValue
+{
+    //Contrutor padrão privado para evitar instanciação da classe.
+    private function  __construct(){}
+
+    // Metódo para recuperar valores das Classes
+    public static function getClassValue($pStrTagLabel, $pStrSource, $pStrTags = '[]')
+    {
+        //Tratar as tags depois!
+        preg_match_all("Win32_" . $pStrTagLabel . "=" . $pStrTagLabel . "",$pStrSource, $arrResult);
+        return $arrResult[1][0];
+    }
+
+    // Metódo para recuperar array com nomes das Classes
+    public static function getClassNames($pStrSource, $pStrTags = '[]')
+    {
+        preg_match_all("^Win32_(.*?)=",$pStrSource,$arrResult);
+        return $arrResult[1];
+    }
+
+    // Metódo para recuperar valores delimitados por tags definidas em  $pStrTags
+    public static function getValueFromTags($pStrTagLabel, $pStrSource, $pStrTags = '[]')
+    {
+        //Tratar as tags depois!
+        preg_match_all("(\[" . $pStrTagLabel . "\](.+)\[\/" . $pStrTagLabel . "\])i",$pStrSource, $arrResult);
+        return $arrResult[1][0];
+    }
+
+    // Metódo para recuperar array com nomes das tags delimitadas por "<" e ">"
+    public static function getTagsFromValues($pStrSource, $pStrTags = '[]')
+    {
+        preg_match_all("/\[\/(.*?)\]/",$pStrSource,$arrResult);
+        return $arrResult[1];
+    }
+
+    // Metódo para excluir uma tag
+    public static function delTags($pStrTagLabel, $pStrSource, $pStrTags = '[]')
+    {
+        $strBeginTag = substr($pStrTags,0,1) 		. $pStrTagLabel . substr($pStrTags,1,1);
+        $strEndTag   = substr($pStrTags,0,1) . '/' 	. $pStrTagLabel . substr($pStrTags,1,1);
+        $strSource	 = $pStrSource;
+
+        $strSource = str_replace($strBeginTag . $strEndTag,'',$strSource);
+
+        while ($strActualValue = getValueFromTags($pStrTagLabel,$strSource))
+            $strSource = str_replace($strBeginTag . $strActualValue . $strEndTag,'',$strSource);
+
+        return $strSource;
+    }
+    // Metódo para atribuir valor a tags
+    public static function setValueToTags($pStrTagLabel, $pStrValue, $pStrSource, $pStrTags = '[]')
+    {
+        $strBeginTag = substr($pStrTags,0,1) 		. $pStrTagLabel . substr($pStrTags,1,1);
+        $strEndTag   = substr($pStrTags,0,1) . '/' 	. $pStrTagLabel . substr($pStrTags,1,1);
+        $strSource	 = $pStrSource;
+
+        $strActualValue = getValueFromTags($pStrTagLabel,$pStrSource);
+        if (stripos2($strSource,$strBeginTag,false))
+            $strSource = str_replace($strBeginTag . $strActualValue . $strEndTag,$strBeginTag . $pStrValue . $strEndTag,$pStrSource);
+        else
+            $strSource .= $strBeginTag . $pStrValue . $strEndTag;
+
+        return $strSource;
+    }
+
+
+}
