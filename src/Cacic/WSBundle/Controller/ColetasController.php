3ab594b75e640f9bd999f91b672ef1bcf7657a23
paulo_borges_ccb@hotmail.com <paulo_borges_ccb@hotmail.com@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-06-27 15:40:58 +0000

    Acrecentado Inserção de computador_coleta e computador_coleta_historico
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1223 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/Cacic/WSBundle/Controller/ColetasController.php
+++ src/Cacic/WSBundle/Controller/ColetasController.php
@@ -3,10 +3,9 @@
 namespace Cacic\WSBundle\Controller;
 
 use Cacic\CommonBundle\Entity\Classe;
-use Cacic\CommonBundle\Entity\ClassProperty;
-use Cacic\CommonBundle\Entity\ClassPropertyType;
 use Cacic\CommonBundle\Entity\ComputadorColeta;
 use Cacic\CommonBundle\Helper\TagValue;
+use Proxies\__CG__\Cacic\CommonBundle\Entity\ComputadorColetaHistorico;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -54,7 +53,7 @@ class ColetasController extends Controller
         $retorno_xml_values	 = '';
 
         // Esta condição responde TRUE para o teste de comunicação efetuado pelo chkCACIC
-        if ( trim( $request->request('in_chkcacic') )=='chkcacic_GetTest' )
+        if ( trim( $request->request('strFieldsAndValuesToRequest') ) == 'in_instalacao=OK' )
             $retorno_xml_values .= '<STATUS>OK</STATUS>';
 
         $retorno_xml = $retorno_xml_header . $retorno_xml_values;
@@ -71,22 +70,34 @@ class ColetasController extends Controller
 
         $coleta = $request->request->get('strFieldsAndValuesToRequest'); //atribuido String coletada a varivel $coleta que será enviado via POST pelo Agente_Cacic
         $te_node_address = TagValue::getValueFromTags( 'MACAddress',TagValue::getClassValue( 'NetworkAdapterConfiguration', $coleta ) ); //extraio MacAdess de coleta para futura compara
-        $classes_property_type = new ClassPropertyType();
-        $classes_property = new ClassProperty();
-        $classe = new Classe();
-        $computador_coleta = new ComputadorColeta();
 
+        $classes = $this->getDoctrine()->getRepository('CacicCommonBundle:Classe')->findAll(); //lista de todas classes
         $computador = $this->getDoctrine()->getRepository('CacicCommonBundle:Computador')->findByTeNodeAddress( $te_node_address ); //pesquiso pelo MacAddress e atribuo o resultado a computador
-        $computador = empty( $computador ) ? new Computador() : $this->computador; // se computador estiver vazio instancio um novo Computador() senão permanecerá o mesmo
+        $computador = empty( $computador ) ? new Computador() : $computador;
 
 
-        $classes_property_coletado = TagValue::getTagsFromValues($coleta); //estraido todas propriedades que foram coletadas pelo Agente_Cacic
-        foreach ($classes_property_coletado as $classe_property)
+        $classes_coletadas = TagValue::getClassNames( $coleta ); //estraido todas classes que foram coletadas pelo Agente_Cacic
+        foreach ( $classes as $classe )
         {
-            $classes_property->setNmPropertyName($classes_property);
-            $classes_property->setIdClass( $classe->getIdClass() ); //verificar como obter classe e ID provavente será necessário passar somente nome.
-            $classes_property_type->setCsType( TagValue::getValueFromTags( $classe_property, $coleta ) ); //passado parametro tag, stringColetada, retorna conteudo coletado na tag passsada por parametro
+            $computador_coleta_historico = new ComputadorColetaHistorico();
 
+            $computador_coleta = this->getDoctrine()->getRepository('CacicCommonBundle:ComputadorColeta')->findByIdComputador( $computador->getIdComputador() ) ; //procura pelo IdComputador
+            $computador_coleta = empty( $computador_coleta ) ? new ComputadorColeta() : $computador_coleta; // se o computador não existir sera instanciado um novo Computador()
+
+            //persistindo coleta de computador
+            $computador_coleta->setIdClass( $classe->getIdClass() );
+            $computador_coleta->setTeClassValues( TagValue::getClassValue( $classe->getNmClassName() ) );
+            $computador_coleta->setIdComputador( $computador->getIdComputador() );
+            $this->getDoctrine()->getManager()->persist( $computador_coleta );
+            $this->getDoctrine()->getManager()->flush();
+
+            //persistendo Historico de Coletas
+            $computador_coleta_historico->setIdComputador( $computador->getIdComputador() );
+            $computador_coleta_historico->setTeClassValues( TagValue::getClassValue( $classe->getNmClassName() ) );
+            $computador_coleta_historico->setIdComputador( $computador->getIdComputador() );
+            $computador_coleta_historico->setDtHrInclusao( \DateTime('NOW') );
+            $this->getDoctrine()->getManager()->persist( $computador_coleta_historico );
+            $this->getDoctrine()->getManager()->flush();
 
         }
     }
