fbc1259e9cc074cb1c0a7824542a49561892678f
paulo_borges_ccb@hotmail.com <paulo_borges_ccb@hotmail.com@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-07-02 20:31:57 +0000

    Caso de Uso Softwares Não Usados
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1240 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/Cacic/CommonBundle/Helper/CommonWs.php
+++ src/Cacic/CommonBundle/Helper/CommonWs.php
@@ -2,22 +2,96 @@
 
 namespace Cacic\CommonBundle\Helper;
 
+use Cacic\CommonBundle\Entity\Computador;
+use Cacic\CommonBundle\Entity\Rede;
 use Cacic\CommonBundle\Helper\Criptografia;
 use Cacic\CommonBundle\Helper\TagValue;
 use Symfony\Component\HttpFoundation\Request;
+use Cacic\CommonBundle\Entity\So;
+use Cacic\CommonBundle\Entity\ComputadorColeta;
 
 class CommonWs {
 
     //Contrutor privado para evitar instanciação da classe.
     private function  __construct(){}
 
-    protected function autenticaAgente($p_PaddingKey='', Request $request)
+    public function checkSoExistInsetNew( $te_so )
     {
-        if( ( strtoupper( Criptografia::deCrypt( $request->request->get('HTTP_USER_AGENT') , $request->request->get('cs_cipher'),  $request->request->get('cs_compress') ,$p_PaddingKey , true ) ) != 'AGENTE_CACIC') ||
-            ( strtoupper( Criptografia::deCrypt( $request->request->get('PHP_AUTH_USER'  ) , $request->request->get('cs_cipher'),  $request->request->get('cs_compress') ,$p_PaddingKey , true ) ) != 'USER_CACIC') ||
-            ( strtoupper( Criptografia::deCrypt( $request->request->get('PHP_AUTH_PW'    ) , $request->request->get('cs_cipher'),  $request->request->get('cs_compress') ,$p_PaddingKey , true ) ) != 'PW_CACIC'))
+        $so = $this->getDoctrine()->getRepository('CacicCommonBundle:So')->findBy( array ( 'te_so' => $te_so ) );
+        if( empty( $so ) )
         {
-            echo 'Acesso não autorizado.'; // deve ser mostrado no browser //verificar Mensagem padrão de erro no Symfony
+            $so = new So();
+            $so->setTeSo($te_so);
+            $so->setSgSo("Sigla a Cadastrar");
+            $so->getTeDescSo("S.O. a Cadastrar");
+            $this->getDoctrine()->getManager()->persist( $so );
+
+            $this->getDoctrine()->getManager()->flush();
+
+        }
+
+        return $so;
+
+    }
+
+    public function getDadosPreColeta( Request $request , $te_so , $te_node_adress, $dt_hr_ult_acesso )
+    {
+        $so = $this->checkSoExistInsetNew( $te_so );
+        $id_so= $so['id_so'];
+
+        $ip = explode( '.', $request->request->get('te_ip_computador') );
+        $te_ip_rede = $ip[0].$ip[1].$ip[2].".0"; //Pega ip da REDE sendo esse X.X.X.0
+
+        $computador = $this->getDoctrine()->getRepository('CacicCommonBundle: Computador')->findBy( array( 'te_node_adress'=> $te_node_adress, 'id_so'=>$id_so));
+        $computador = empty($computador) ? new Computador() : $computador;
+        $rede =  $this->getDoctrine()->getRepository('CacicCommonBundle: Rede')->findBy( array( 'te_ip_rede'=> $te_ip_rede ) );
+
+        if( empty($computador['dt_hr_inclusao']) )
+        {
+            $computador->setTeNodeAddress( $te_node_adress );
+            $computador->getIdSo( $id_so );
+            $computador->setIdRede( $rede['id_rede'] );
+            $computador->setDtHrInclusao( \DateTime('NOW') );
+        }
+
+        $computadorColeta = new ComputadorColeta();
+        $computadorColeta->setIdComputador( $computador->getIdComputador() );
+        $computadorColeta->setTeClassValues( $request->request->get('NetworkAdapterConfiguration') );
+        $computadorColeta->setIdClass(
+            $this->getDoctrine()->getRepository('CacicCommonBundle: Classe')->findByIdClass( array( 'nm_class_name'=> 'NetworkAdapterConfiguration') )
+        );
+        $this->getDoctrine()->getManager()->persist( $computadorColeta );
+
+        $computadorColeta = new ComputadorColeta();
+        $computadorColeta->setIdComputador( $computador->getIdComputador() );
+        $computadorColeta->setTeClassValues( $request->request->get('OperatingSystem') );
+        $computadorColeta->setIdClass(
+            $this->getDoctrine()->getRepository('CacicCommonBundle: Classe')->findByIdClass( array( 'nm_class_name'=> 'OperatingSystem') )
+        );
+        $this->getDoctrine()->getManager()->persist( $computadorColeta );
+
+        $computadorColeta = new ComputadorColeta();
+        $computadorColeta->setIdComputador( $computador->getIdComputador() );
+        $computadorColeta->setTeClassValues( $request->request->get('ComputerSystem') );
+        $computadorColeta->setIdClass(
+            $this->getDoctrine()->getRepository('CacicCommonBundle: Classe')->findByIdClass( array( 'nm_class_name'=> 'ComputerSystem') )
+        );
+        $this->getDoctrine()->getManager()->persist( $computadorColeta );
+
+        $computador->setDtHrUltAcesso( \DateTime('NOW') );
+        $computador->setTeVersaoCacic( $request->request->get('te_versao_cacic') );
+        $computador->setTeVersaoGercols( $request->request->get('te_versao_gercols') );
+
+
+    }
+
+    public static function autenticaAgente($p_PaddingKey='', Request $request)
+    {
+        if( ( strtoupper( Criptografia::deCrypt( $request, $request->request->get('HTTP_USER_AGENT') , $request->request->get('cs_cipher'),  $request->request->get('cs_compress') ,$p_PaddingKey , true ) ) != 'AGENTE_CACIC') ||
+            ( strtoupper( Criptografia::deCrypt( $request, $request->request->get('PHP_AUTH_USER'  ) , $request->request->get('cs_cipher'),  $request->request->get('cs_compress') ,$p_PaddingKey , true ) ) != 'USER_CACIC') ||
+            ( strtoupper( Criptografia::deCrypt( $request, $request->request->get('PHP_AUTH_PW'    ) , $request->request->get('cs_cipher'),  $request->request->get('cs_compress') ,$p_PaddingKey , true ) ) != 'PW_CACIC'))
+        {
+            echo 'Acesso Não Autorizado.'; // deve ser mostrado no browser //verificar Mensagem padrão de erro no Symfony
         }
     }
 
@@ -36,11 +110,11 @@ class CommonWs {
         $boolAgenteLinux = ( trim( $request->request->get('AgenteLinux') ) <> '' ? true : false );
 
         // Autenticação da chamada:
-        autenticaAgente( $strPaddingKey );
+        self::autenticaAgente( $strPaddingKey, $request );
 
-        $strNetworkAdapterConfiguration  = Criptografia::deCrypt( $request->request->get('NetworkAdapterConfiguration') , $v_cs_cipher, $v_cs_compress,$strPaddingKey );
-        $strComputerSystem  			 = Criptografia::deCrypt( $request->request->get('ComputerSystem')				, $v_cs_cipher, $v_cs_compress,$strPaddingKey );
-        $strOperatingSystem  			 = Criptografia::deCrypt( $request->request->get('OperatingSystem')			    , $v_cs_cipher, $v_cs_compress,$strPaddingKey );
+        $strNetworkAdapterConfiguration  = Criptografia::deCrypt( $request, $request->request->get('NetworkAdapterConfiguration')   , $v_cs_cipher, $v_cs_compress,$strPaddingKey );
+        $strComputerSystem  			 = Criptografia::deCrypt( $request, $request->request->get('ComputerSystem')				, $v_cs_cipher, $v_cs_compress,$strPaddingKey );
+        $strOperatingSystem  			 = Criptografia::deCrypt( $request, $request->request->get('OperatingSystem')			    , $v_cs_cipher, $v_cs_compress,$strPaddingKey );
 
         $arrDadosComputador 			 = getDadosComputador(
             TagValue::getValueFromTags('MACAddress', $strNetworkAdapterConfiguration), // reescrever getDadosComputador no Library
@@ -51,7 +125,7 @@ class CommonWs {
         $arrDadosRede 					 = getDadosRede($arrDadosComputador[0]['id_rede']); // reescrever getDadosRede no Library
 
         if ( $request->request->get('te_palavra_chave') )
-            $strTePalavraChave = Criptografia::deCrypt( $request->request->get('te_palavra_chave') , $v_cs_cipher,$v_cs_compress,$strPaddingKey );
+            $strTePalavraChave = Criptografia::deCrypt( $request, $request->request->get('te_palavra_chave') , $v_cs_cipher,$v_cs_compress,$strPaddingKey );
 
         // --------------- Retorno de Classificador de CRIPTOGRAFIA --------------------------------------------- //
         if ($v_cs_cipher <> '1') $v_cs_cipher --;
