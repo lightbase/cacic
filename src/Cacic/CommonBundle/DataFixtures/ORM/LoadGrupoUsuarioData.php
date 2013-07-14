f2bf2225ff76a728e0faa05d3777e83718f4eff8
eduardo.santos <eduardo.santos@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-07-14 01:18:00 +0000

    Carrega Fixture com usuário padrão
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1293 fecfc0c7-e812-0410-ae72-849f08638ee7

---
+++ src/Cacic/CommonBundle/DataFixtures/ORM/LoadGrupoUsuarioData.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: eduardo
+ * Date: 13/07/13
+ * Time: 21:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Hotels\CoreBundle\DataFixtures\ORM;
+
+use Cacic\CommonBundle\Entity\GrupoUsuario;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/*
+ * Carrega grupos de usuário
+ */
+
+class LoadGrupoUsuarioData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
+{
+
+    /*
+     * Carrega grupo admin
+     */
+
+    public function load(ObjectManager $manager)
+    {
+        $grupoAdmin = new GrupoUsuario();
+        $grupoAdmin->setNmGrupoUsuarios('Admin');
+        $grupoAdmin->setTeGrupoUsuarios('Administradores');
+        $grupoAdmin->setTeMenuGrupo('menu_adm.txt');
+        $grupoAdmin->setTeDescricaoGrupo('Acesso irrestrito');
+        $grupoAdmin->setCsNivelAdministracao(true);
+
+        // Adiciona referência ao Grupo que será usada depois
+        $this->addReference('grupo-admin', $grupoAdmin);
+
+        $manager->persist($grupoAdmin);
+        $manager->flush();
+
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+}
---
+++ src/Cacic/CommonBundle/DataFixtures/ORM/LoadLocalData.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: eduardo
+ * Date: 13/07/13
+ * Time: 21:25
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Hotels\CoreBundle\DataFixtures\ORM;
+
+use Cacic\CommonBundle\Entity\Local;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/*
+ * Carrega local para teste
+ */
+
+class LoadLocalData extends AbstractFixture implements FixtureInterface,  OrderedFixtureInterface
+{
+
+    /*
+     * Carrega local padrão
+     *
+     */
+
+    public function load(ObjectManager $manager)
+    {
+        $local = new Local();
+        $local->setNmLocal('Empresa');
+        $local->setSgLocal('LTDA');
+
+        // Adiciona referência ao local que será carregado depois
+        $this->addReference('local', $local);
+
+        $manager->persist($local);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
