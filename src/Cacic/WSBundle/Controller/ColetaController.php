ffc57b3ea958d30e545e181cde0165b8603a7a20
paulo_borges_ccb@hotmail.com <paulo_borges_ccb@hotmail.com@fecfc0c7-e812-0410-ae72-849f08638ee7>
2013-08-09 12:16:03 +0000

    Iniciado Reescrita mapaCacicAcessoAction()
    git-svn-id: http://svn.softwarepublico.gov.br/svn/cacic/cacic/branches/3.0/gerente@1375 fecfc0c7-e812-0410-ae72-849f08638ee7

--- src/Cacic/WSBundle/Controller/ColetaController.php
+++ src/Cacic/WSBundle/Controller/ColetaController.php
@@ -306,6 +306,69 @@ class ColetaController extends Controller
             fwrite( $fp, "[{$postKey}]: {$postVal}\n");
         }
         fclose($fp);
+        $strXML_Values = '';
+        if ( trim(OldCacicHelper::deCrypt($request, $request->get('ModuleProgramName')) == 'mapacacic.exe') )
+        {
+            if ($request->get('te_operacao') == 'CheckVersion')
+            {
+                if (file_exists(OldCacicHelper::CACIC_PATH . OldCacicHelper::CACIC_PATH_RELATIVO_DOWNLOADS . 'versions_and_hashes.ini'))
+                {
+                    $arrVersionsAndHashes = parse_ini_file(OldCacicHelper::CACIC_PATH . OldCacicHelper::CACIC_PATH_RELATIVO_DOWNLOADS . 'versions_and_hashes.ini');
+
+                    if ($arrVersionsAndHashes['mapacacic.exe_HASH'] <> $request->get('MAPACACIC_EXE_HASH'))
+                        $strXML_Values	 	.= '[MAPACACIC.EXE_HASH]'.$arrVersionsAndHashes['mapacacic.exe_HASH'].'[/MAPACACIC.EXE_HASH]';
+                }
+            }
+            elseif ($request->get('te_operacao') == 'Autentication')
+            {
+                // Autenticação do agente MapaCacic.exe:
+                OldCacicHelper::autenticaAgente($request);
+
+                // Essa condição testa se a chamada trouxe o valor de cs_mapa-cacic, enviado por MapaCacic.exe
+                if (trim(OldCacicHelper::deCrypt($request,$request->get('ModuleProgramName')) == 'mapacacic.exe'))
+                {
+//                    $strSelect 	= "	a.id_usuario,
+//							a.nm_usuario_completo,
+//							a.id_local,
+//							a.te_locais_secundarios,
+//							c.sg_local";
+//
+//                    $strFrom	= "	usuarios a,
+//							locais c";
+//
+//                    $strWhere 	= "	(a.id_local = c.id_local OR ('," . $arrDadosRede[0]['id_local'] . ",' in (CONCAT(',',a.te_locais_secundarios,',')))) AND
+//							a.nm_usuario_acesso = '". trim(DeCrypt($request->get('nm_acesso'],$v_cs_cipher,$v_cs_compress,$strPaddingKey)) ."' AND
+//							a.te_senha = ";
+
+                    // Solução temporária, até total convergência para vers�es 4.0.2 ou maior de MySQL
+                    // Anderson Peterle - Dataprev/ES - 04/09/2006
+//                    $v_AUTH_SHA1 	 = " SHA1('". trim(DeCrypt($request->get('te_senha'],$v_cs_cipher,$v_cs_compress,$strPaddingKey)) ."')";
+//                    $v_AUTH_PASSWORD = " PASSWORD('". trim(DeCrypt($request->get('te_senha'],$v_cs_cipher,$v_cs_compress,$strPaddingKey)) ."')";
+
+                    // Para MySQL 4.0.2 ou maior
+                    // Anderson Peterle - Dataprev/ES - 04/09/2006
+//                    $arrUsuario = getArrFromSelect( $strFrom,
+//                        $strSelect,
+//                        $strWhere . $v_AUTH_SHA1);
+
+                    if ($arrUsuario[0]['id_usuario'] == '')
+                    {
+                        // Para MySQL at� 4.0
+                        // Anderson Peterle - Dataprev/ES - 04/09/2006
+//                        $arrUsuario = getArrFromSelect( $strFrom,
+//                            $strSelect,
+//                            $strWhere . $v_AUTH_PASSWORD);
+                    }
+
+                    if ($arrUsuario[0]['id_usuario'] <> '')
+                    {
+//                        $strXML_Values .= '[ID_USUARIO]'			. EnCrypt($arrUsuario[0]['id_usuario'],$v_cs_cipher,$v_cs_compress,$v_compress_level,$strPaddingKey) . '[/ID_USUARIO]';
+//                        $strXML_Values .= '[NM_USUARIO_COMPLETO]'	. EnCrypt($arrUsuario[0]['nm_usuario_completo'].' ('.$arrUsuario[0]['sg_local'].')',$v_cs_cipher,$v_cs_compress,$v_compress_level,$strPaddingKey) . '[/NM_USUARIO_COMPLETO]';
+//                        GravaLog('ACE',$_SERVER['SCRIPT_NAME'],'acesso',$arrUsuario[0]["id_usuario"]);
+                    }
+                }
+            }
+        }
 
     }
 
