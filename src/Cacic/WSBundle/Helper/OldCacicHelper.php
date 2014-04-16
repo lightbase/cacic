e197367840a0cbe30b0d7f17f2ff294064b77018
Eduardo Santos <eduardo.edusantos@gmail.com>
2014-04-16 15:48:45 -0300

    Refatoração das coletas para aumentar a eficiência.

--- src/Cacic/WSBundle/Helper/OldCacicHelper.php
+++ src/Cacic/WSBundle/Helper/OldCacicHelper.php
@@ -230,4 +230,14 @@ class OldCacicHelper
         return $intPos;
     }
 
+    public static function  udate($format = 'u', $utimestamp = null) {
+        if (is_null($utimestamp))
+            $utimestamp = microtime(true);
+
+        $timestamp = floor($utimestamp);
+        $milliseconds = round(($utimestamp - $timestamp) * 1000000);
+
+        return date(preg_replace('`(?<!\\\\)u`', $milliseconds, $format), $timestamp);
+    }
+
 }
