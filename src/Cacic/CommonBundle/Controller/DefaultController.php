ddedf0443d3560f54b7fd5e6f4d22abb140dc8f3
Bruno Menezes <bruno.noronha@lightbase.com.br>
2014-07-02 14:54:07 -0300

    incluindo comparação de 7 dias em maquinas acessadas

--- src/Cacic/CommonBundle/Controller/DefaultController.php
+++ src/Cacic/CommonBundle/Controller/DefaultController.php
@@ -17,8 +17,10 @@ class DefaultController extends Controller
 			'totalCompMonitorados' => $this->getDoctrine()->getRepository('CacicCommonBundle:Computador')->countAll(),
 			'totalInsucessosInstalacao' => $this->getDoctrine()->getRepository('CacicCommonBundle:InsucessoInstalacao')->countAll(),
 			'totalCompPorSO' => $this->getDoctrine()->getRepository('CacicCommonBundle:Computador')->countPorSO30Dias(),
-			'totalComp' => $this->getDoctrine()->getRepository('CacicCommonBundle:LogAcesso')->countPorComputador()
-		);
+			'totalComp' => $this->getDoctrine()->getRepository('CacicCommonBundle:LogAcesso')->countPorComputador(),
+            'totalComp7Dias' => $this->getDoctrine()->getRepository('CacicCommonBundle:LogAcesso')->countComputadorDias('0','7'),
+            'totalComp14Dias' => $this->getDoctrine()->getRepository('CacicCommonBundle:LogAcesso')->countComputadorDias('7','14')
+        );
 		
 		return $this->render(
 			'CacicCommonBundle:Default:index.html.twig',
