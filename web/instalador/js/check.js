be093332d1b3f13761b06a67d78cc1ed9247ddaa
Eduardo Santos <eduardo.edusantos@gmail.com>
2014-11-05 16:47:02 -0200

    Import de correções na coleta para implementação na SDH

--- web/instalador/js/check.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-	if(document.terms.lido.checked) {
-	    document.getElementById("go").innerHTML="<input class='btn btn-default' type='submit' value='Continuar'>";
-	} else {
-	    document.getElementById("go").innerHTML="<input class='btn btn-default' type='submit' value='Continuar' disabled='true'>";
-	}
-};
