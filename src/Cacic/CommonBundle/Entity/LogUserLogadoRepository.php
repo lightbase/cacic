b8d8857538e3f5b11a2f17aab1d7c87b29ad3ffe
Bruno Noronha <bruno.noronha@lightbase.com.br>
2014-11-10 10:27:04 -0200

    Relatório de usuário logado

--- src/Cacic/CommonBundle/Entity/LogUserLogadoRepository.php
+++ src/Cacic/CommonBundle/Entity/LogUserLogadoRepository.php
@@ -5,15 +5,14 @@ namespace Cacic\CommonBundle\Entity;
 use Doctrine\ORM\EntityRepository;
 
 /**
- * LogAcessoRepository
+ * LogUserLogadoRepository
  *
  * Métodos de repositório
  */
 class LogUserLogadoRepository extends EntityRepository
 {
-    /**
+    /*
      * Função que retorna o último acesso para o computador solicitado
-     *
      * @param $computador
      */
     public function ultimoAcesso( $computador ) {
@@ -27,4 +26,36 @@ class LogUserLogadoRepository extends EntityRepository
         return $qb->getQuery()->getOneOrNullResult();
     }
 
+    /*
+    * Função que retorna a busca por usuário logago
+    */
+    public function selectUserLogado( $teIpComputador , $nmComputador ,$usuario, $dtHrInclusao ,$dtHrInclusaoFim )
+    {
+
+        $query = $this->createQueryBuilder('c')
+            ->select('c.nmComputador', 'c.teIpComputador', 'log.data', 'log.usuario')
+            ->from('computador', 'c')
+            ->innerJoin('log.idComputador', 'WITH', 'c.idComputador');
+
+        if ( $teIpComputador != null){
+
+            $query->Where("c.teIpComputador  LIKE   '%$teIpComputador%'");
+
+        }
+        if ( $nmComputador != null){
+            $query->Where("c.nmComputador LIKE   '%$nmComputador%'");
+        }
+        if ( $usuario != null){
+            $query->Where("log.usuario  LIKE   '%$usuario%'");
+        }
+        if ( $dtHrInclusao != null){
+            $query->andWhere( 'log.data >= (:dtHrInclusao)' )->setParameter('dtHrInclusao', ( $dtHrInclusao.' 00:00:00' ));
+        }
+        if ( $dtHrInclusaoFim != null){
+            $query->andWhere( 'log.data<= (:dtHrInclusaoFim)' )->setParameter('dtHrInclusaoFim', ( $dtHrInclusaoFim.' 23:59:59' ));
+        }
+
+
+        return $query->getQuery()->execute();
+    }
 }
