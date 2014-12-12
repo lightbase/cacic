dcd7f6a627727231cc5ecb625b2c3fb812af3729
Eduardo Santos <eduardo.edusantos@gmail.com>
2014-12-12 13:56:59 -0200

    Migration para o relatório de licenças de software funcionar corretamente.

---
+++ src/Cacic/CommonBundle/DoctrineMigrations/Version20141017220727.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cacic\CommonBundle\Migrations;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20141017220727 extends AbstractMigration implements ContainerAwareInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $logger = $this->container->get('logger');
+        $this->addSql("ALTER TABLE grupo_usuario ADD role TEXT DEFAULT NULL");
+
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
