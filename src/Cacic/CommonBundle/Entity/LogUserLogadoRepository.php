30c39cf030270796a2c969dceb49342e6535bad3
MauroSantos <spokeste@gmail.com>
2015-02-25 13:02:41 -0300

    Correção pesquisa user estático

--- src/Cacic/CommonBundle/Entity/LogUserLogadoRepository.php
+++ src/Cacic/CommonBundle/Entity/LogUserLogadoRepository.php
@@ -182,7 +182,7 @@ WHERE  1 = 1
         }
 
         if ( $usuarioPatrimonio ) {
-            $sql .= " AND cp_.nm_property_name = 'UserName' AND lower(cc_.te_class_property_value) LIKE lower('%$usuarioPatrimonio%') ";
+            $sql .= " AND cp_.nm_property_name = 'UserName' AND lower(cc_.te_class_property_value) LIKE lower('%$usuarioPatrimonio%')";
         }
 
         if ( $usuarioName ) {
