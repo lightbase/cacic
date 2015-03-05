d49c7b4fc956d9641cac1b6c1a80bed39fef61a9
Bruno Menezes <bruno.noronha@lightbase.com.br>
2015-03-05 17:01:13 -0300

    csv user logado completo sem patrimonio

--- src/Cacic/RelatorioBundle/Controller/FaturamentoController.php
+++ src/Cacic/RelatorioBundle/Controller/FaturamentoController.php
@@ -656,9 +656,9 @@
             $file = new \SplFileObject($tmpfile, 'w');
             $writer = new CsvWriter($file);
             if ($semData == 'S'){
-                $writer->writeItem(array( 'Nome computador', 'Mac Address','IP computador','Local', 'Sub Rede', 'Usuário', 'Data', 'IP Computador'));
+                $writer->writeItem(array('Nome computador', 'Mac Address','IP computador','Local', 'Sub Rede', 'Usuário', 'Data', 'IP Computador'));
             }else{
-                $writer->writeItem(array( 'Nome computador', 'Mac Address','IP computador','Local', 'Sub Rede', 'CPF Responsável', 'Data Pup-up', 'Usuário', 'Data', 'IP Computador'));
+                $writer->writeItem(array('Nome computador', 'Mac Address','IP computador','Local', 'Sub Rede', 'CPF Responsável', 'Data Pup-up', 'Usuário', 'Data', 'IP Computador'));
             }
             $workflow->addWriter($writer);
 
