5c89ce94f8020497a2ce52c67f07f6f27e77f86b
Eduardo Santos <eduardo@eduardosan.com>
2016-03-15 16:59:52 -0300

    Verdadeiro como padrão.

---
+++ src/Cacic/CommonBundle/DoctrineMigrations/Version20160315165707.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cacic\CommonBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160315165707 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE computador_coleta set ativo = 't' where ativo IS NULL");
+        $this->addSql("UPDATE proriedade_software set ativo = 't' where ativo IS NULL");
+        $this->addSql("UPDATE class_property set ativo = 't' where ativo IS NULL");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
